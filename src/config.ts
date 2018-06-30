628f409a66f4ee7376e23614e011f96a8cfa822e
mygoodkab <mygood_kab@hotmail.com>
2018-06-30 14:40:40 +0700

    first commit

---
+++ src/config.ts
@@ -0,0 +1,33 @@
+
+import * as pathSep from 'path';
+module.exports = {
+    dev: {
+        path: {
+            upload: pathSep.join(__dirname, 'uploads'),
+            pdf: pathSep.join(__dirname, 'uploads', 'document.pdf'),
+        },
+        hapi: {
+            router: { routes: 'dist/routes/*.js' }
+        },
+        fileType: {
+            images: [
+                'png',
+                'jpg',
+                'jpeg',
+            ],
+            pdf: ['pdf'],
+        },
+        jwt: {
+            timeout: '8h',
+            refreshInterval: 30 * 60 * 1000 // 30 mins
+        },
+        timezone: {
+            thai: 7 * 60 * 60 * 1000
+        },
+        regex: /[\S]+/,
+        mail: {
+            DOMAIN: `mg.codth.com`,
+            API_KEY: `key-b09165d35576ee942a4158800f0282af`,
+        },
+    }
+};
